871e15237d7441b54efd36fe60a061ba9838390a
Taha Cini <taha.cini@outlook.com>
2022-01-01 14:13:54 +0300

    file created

---
+++ roman-numeral-converter.js
@@ -0,0 +1,22 @@
+function convertToRoman(num) {
+  // Basically we have two array containing the main roman numbers and its numerical values.
+  const romanValue = [
+    "M",
+    "CM",
+    "D",
+    "CD",
+    "C",
+    "XC",
+    "L",
+    "XL",
+    "X",
+    "IX",
+    "V",
+    "IV",
+    "I",
+  ];
+  const value = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let romanNum = "";
+}
+
+convertToRoman(36);
