af008374cdfea91972e2c11f68c876fd47df72ba
Taha Cini <taha.cini@outlook.com>
2022-01-02 10:55:38 +0300

    File added

---
+++ caesars-cipher.js
@@ -0,0 +1,35 @@
+function rot13(str) {
+  // Splits the word by each letter
+  const word = str.split("");
+  // Rot13 object for decoding
+  const rot13 = {
+    A: "N",
+    B: "O",
+    C: "P",
+    D: "Q",
+    E: "R",
+    F: "S",
+    G: "T",
+    H: "U",
+    I: "V",
+    J: "W",
+    K: "X",
+    L: "Y",
+    M: "Z",
+    N: "A",
+    O: "B",
+    P: "C",
+    Q: "D",
+    R: "E",
+    S: "F",
+    T: "G",
+    U: "H",
+    V: "I",
+    W: "J",
+    X: "K",
+    Y: "L",
+    Z: "M",
+  };
+}
+
+rot13("SERR PBQR PNZC");
